882e6fd376557307a0cb009b94be7f473acf776d
Apple <apple@redlimesolutions.com>
2022-07-22 15:18:55 +0600

    map, dynamic stopage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +6,7 @@ import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  @ViewChild('mapContainer', {static: false}) gmap!: ElementRef;
+
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
 
 import { AppComponent } from './app.component';
+import { RideSettingComponent } from './components/ride-setting/ride-setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RideSettingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    GooglePlaceModule
   ],
   providers: [],
   bootstrap: [AppComponent]

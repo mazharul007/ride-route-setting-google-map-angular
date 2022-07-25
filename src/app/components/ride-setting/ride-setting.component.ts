851ec44020fe745c0071fb90d507efcb84f3b252
Apple <apple@redlimesolutions.com>
2022-07-25 16:46:06 +0600

    pickup and drop off location added

--- src/app/components/ride-setting/ride-setting.component.ts
+++ src/app/components/ride-setting/ride-setting.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Options } from 'sortablejs';
 import { mapStyle } from './mapStyle';
 
 @Component({
@@ -11,6 +12,7 @@ export class RideSettingComponent implements OnInit, AfterViewInit {
   @ViewChild('mapContainer', { static: false }) gmap!: ElementRef;
 
   waypts: google.maps.DirectionsWaypoint[] = [];
+
   locationFields: any[] = [
     { name: 'pickup', value: '' },
     {
@@ -18,11 +20,15 @@ export class RideSettingComponent implements OnInit, AfterViewInit {
     }
   ]
 
-  // draggable = {
-  //   data: this.locationFields,
-  //   effectAllowed: "all",
-  //   handle: false
-  // }
+  pickAddLat: any;
+  pickAddLng: any;
+  dropAddLat: any;
+  dropAddLng: any;
+
+
+  options: Options = {
+    animation: 150
+  };
 
 
   map!: google.maps.Map;
@@ -36,7 +42,7 @@ export class RideSettingComponent implements OnInit, AfterViewInit {
   directionsDisplay = new google.maps.DirectionsRenderer({ polylineOptions: { strokeColor: "#8DC53E" }, suppressMarkers: true, preserveViewport: true });
 
 
-  public stopCoorArr: google.maps.LatLngLiteral[] = [];
+  // public stopCoorArr: google.maps.LatLngLiteral[] = [];
 
   mapOptions: google.maps.MapOptions = {
     center: this.coordinates,
@@ -58,7 +64,16 @@ export class RideSettingComponent implements OnInit, AfterViewInit {
 
 
 
-  constructor() { }
+  constructor() {
+    this.options = {
+      onUpdate: (event: any) => {
+        console.log(event);
+        console.log(this.locationFields)
+        return false;
+      }
+    }
+
+  }
 
   ngAfterViewInit(): void {
     this.mapInitializer();
@@ -69,17 +84,125 @@ export class RideSettingComponent implements OnInit, AfterViewInit {
   }
 
   mapInitializer() {
+    this.mapOptions = {
+      ...this.mapOptions,
+      center: this.coordinates
+    }
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    this.initMarkers();
+  }
 
-    this.map = new google.maps.Map(this.gmap.nativeElement,
-      this.mapOptions);
+  initMarkers() {
     for (let i = 0; i < this.markers.length; i++) {
       this.markers[i].setMap(this.map)
       this.directionsDisplay.setMap(this.map);
     }
   }
 
+  setmarker(coordinates: any, i: number) {
+
+    const newMarker = new google.maps.Marker({
+      position: coordinates
+    });
+    if (i == 0) {
+      this.markers.unshift(newMarker);
+    }
+    if (i == (this.locationFields.length - 1)) {
+      this.markers.push(newMarker);
+    }
+
+    // centerise map to marker
+    this.map.panTo(coordinates);
+
+  }
+
   addressChanged(address: any, i: number) {
     console.log(address, i);
+    if (i == 0) {
+      const oldPickAddLat = this.pickAddLat;
+      this.pickAddLat = address.geometry.location.lat();
+      this.pickAddLng = address.geometry.location.lng();
+      this.coordinates = new google.maps.LatLng(this.pickAddLat, this.pickAddLng);
+      this.setmarker(this.coordinates, i);
+
+      if (oldPickAddLat) {
+        this.markers[1].setMap(null);
+        this.markers.splice(1, 1);
+        this.initMarkers();
+      }
+      else {
+        this.initMarkers();
+      }
+
+      this.calculateAndDisplayRoute(this.directionsService, this.directionsDisplay);
+    }
+    if (i == (this.locationFields.length - 1)) {
+      const oldDropAddLat = this.dropAddLat;
+      this.dropAddLat = address.geometry.location.lat();
+      this.dropAddLng = address.geometry.location.lng();
+      this.coordinates = new google.maps.LatLng(this.dropAddLat, this.dropAddLng);
+      this.setmarker(this.coordinates, i);
+      if (oldDropAddLat) {
+        if (this.pickAddLat) {
+          console.log(this.markers);
+          this.markers[this.markers.length - 2].setMap(null);
+          this.markers.splice((this.markers.length - 2), 1);
+          console.log(this.markers);
+          this.initMarkers();
+        } else {
+          this.markers[0].setMap(null);
+          this.markers.shift();
+          this.initMarkers();
+        }
+      }
+      else {
+        this.initMarkers();
+      }
+
+      this.calculateAndDisplayRoute(this.directionsService, this.directionsDisplay);
+    }
+  }
+
+  calculateAndDisplayRoute(directionService: any, directionsDisplay: any): void {
+
+    let start, end;
+
+    if (this.pickAddLat) {
+      const pickupLat: number = +this.pickAddLat;
+      const pickupLng: number = +this.pickAddLng;
+      start = new google.maps.LatLng(pickupLat, pickupLng);
+
+      if (this.dropAddLat) {
+        const dropLat: number = +this.dropAddLat;
+        const dropLng: number = +this.dropAddLng;
+        end = new google.maps.LatLng(dropLat, dropLng);
+      }
+      else {
+        return
+      }
+    }
+    else {
+      return
+    }
+
+    this.directionsService.route({
+      origin: start,
+      destination: end,
+      optimizeWaypoints: false,
+      avoidTolls: false,
+      waypoints: [] = this.waypts,
+      travelMode: google.maps.TravelMode.DRIVING
+    },
+      function (response, status) {
+        if (status == 'OK') {
+          directionsDisplay.setDirections(response);
+        }
+        else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      }
+    );
+
   }
 
   removeStop(i: number) {
@@ -87,8 +210,9 @@ export class RideSettingComponent implements OnInit, AfterViewInit {
   }
 
   addStop() {
-    const newStop = { name: 'stopLocation', value: '' }
-    this.locationFields.push(newStop);
+    const newStop = { name: 'stopLocation', value: '' };
+    const toIndex = this.locationFields.length - 1;
+    this.locationFields.splice(toIndex, 0, newStop);
 
   }
 
